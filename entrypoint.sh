a056449150e2cdeacaa25429c9d3df50277eac25
Brandon Smith <bsmithcompsci@gmail.com>
2024-05-29 01:50:58 -0700

    fix: entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
+#! /bin/bash
+
+# echo everything out.
+set -x
+
+# echo out all environment variables
+env
+
+# Copy the workspace to the /app directory
 cp -r . /app/workspace
 
+# Change to the workspace directory
 cd /app/workspace
 
+# Run the application
 /app/flexvers --skip-non-formatted
