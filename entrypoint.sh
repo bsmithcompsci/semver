96b3b8dcb187adbe5882bcb449eadc5710dcffea
Brandon Smith <bsmithcompsci@gmail.com>
2024-05-29 02:42:40 -0700

    fix: entrypoint.sh script not copying correctly, and too much.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 # echo everything out.
 set -x
@@ -6,11 +6,20 @@ set -x
 # echo out all environment variables
 env
 
-# Copy the workspace to the /app directory
-cp -r . /app/workspace
+echo "Current directory: $(pwd)"
+ls -aln /github/workspace
+
+# Copy the workspace to the /app/workspace directory
+mkdir -p /app/workspace
+cp -r $(pwd)/.git /app/workspace/.git
+cp -r $(pwd)/.semver.json /app/workspace/.semver.json
+
+chown -R 1:1 /app/workspace
+ls -aln /app/workspace
 
 # Change to the workspace directory
 cd /app/workspace
+ls -aln /app
 
 # Run the application
 /app/flexvers --skip-non-formatted
