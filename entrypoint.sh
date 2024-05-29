b38d72dc7c57309de0a429fbd61d61266c283a8c
Brandon Smith <bsmithcompsci@gmail.com>
2024-05-29 03:06:01 -0700

    chore: cleanup and arguments for entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 
-# echo everything out.
-set -x
-
-# echo out all environment variables
-env
-
-echo "Current directory: $(pwd)"
-ls -aln /github/workspace
-
 # Copy the workspace to the /app/workspace directory
 mkdir -p /app/workspace
 cp -r $(pwd)/.git /app/workspace/.git
 cp -r $(pwd)/.semver.json /app/workspace/.semver.json
 
 chown -R $(id -u):$(id -g) /app/workspace
-ls -aln /app/workspace
 
 # Change to the workspace directory
 cd /app/workspace
-ls -aln /app
+
+# Construct Args
+args=""
+
+# Handling the variables:
+# INPUT_SKIP-NON-FORMATTED
+# INPUT_KEEP-ROOT-VERSION-UP-TO-DATE
+# INPUT_FORCE-RELEASE
+# INPUT_FORCE-PRE-RELEASE
+# Check if the --skip-non-formatted flag environment variable is true
+if [ "${INPUT_SKIP-NON-FORMATTED}" = "true" ]; then
+    echo "Skipping non-formatted"
+    args="--skip-non-formatted"
+fi
+if [ "${INPUT_KEEP-ROOT-VERSION-UP-TO-DATE}" = "true" ]; then
+    echo "Keeping root version up to date"
+#   args="$args --keep-root-version-up-to-date"
+fi
+if [ "${INPUT_FORCE-RELEASE}" = "true" ]; then
+    echo "Forcing release"
+#   args="$args --force-release"
+fi
+if [ "${INPUT_FORCE-PRE-RELEASE}" = "true" ]; then
+    echo "Forcing pre-release"
+#   args="$args --force-pre-release"
+fi
 
 # Run the application
-/app/flexvers --skip-non-formatted
+/app/flexvers $args
